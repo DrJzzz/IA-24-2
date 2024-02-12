b1eeb676b161a318deda49a1d57ee0379dc9b382
DrJzzz <josue9499@gmail.com>
2024-02-12 13:15:29 -0600

    restored  file to original to fix indentation issues

--- chatBot/01/bot.py
+++ chatBot/01/bot.py
@@ -1,55 +1,38 @@
 import discord
 import respuestas
 
-
-async def send_message(message, user_message, is_private): // al autor se le manda el mensaje  y mesange contiene muchos
-    response=respuestas.get_response(user_massage)
+async def send_message(message, user_message, is_private):
+    response = respuestas.get_response(user_message)
     await message.author.send(response) if is_private else await message.channel.send(response)
 
-//esta funcion es la magica
 def run_discord_bot():
-    TOKEN='AQUI PONEMOS EL TOKEN DEL BOT'
-    intents=discord.Intents.default()
-    intents.message_content=True
-    client=discord.Client(intents=intents)    // osea que esto sera el contenido del mensaje
+    #Token should be stored in an .envrc file
+    TOKEN = 'MTIwNjY2Nzc0MTM2MTIwOTQxNQ.GiTJd-.5avyZrvXiYrfHUi1dMCnRpZlKa_gt0Uj0uFVyc'
+    intents = discord.Intents.default()
+    intents.message_content = True
+    client = discord.Client(intents=intents)
 
-@client.event  // esta funcion servira para saber que el bot funciono sin problema 
-async def on_ready():
-    print(f'{client.user} Esta vivo!!! ')
+    @client.event
+    async def on_ready():
+        print(f'{client.user} Está vivo!!')
 
-@client.event  // esta funcion recibira los mensajes
-async def on_message(message):   // en estas lineas verifican que el autor del mensaje no sea el mismo bot 
-    if message.author== client.user:
-        return 
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
 
-// esto es por si acaso no lo ocupara ahorita pero por si lo queriamos 
-//ahi esta 
-//dice que el str sirve para limpiar la cadena como de idioma o caracteres para no romper el bot 
-username=str(message.author) 
-user_message=str (message.content)
-channel=str (message.channel)
+        ##Información útil, usuario del mensaje, mensaje, y canal.
+        username = str(message.author)
+        user_message = str(message.content)
+        channel = str(message.channel)
 
+        print(f'{username} mandó: "{user_message}" ({channel})')
 
-print(f'{username}mando: "{user_message}"({channel })')
+        ##Para conversación en privado
+        if user_message[0] == '?':
+            user_message = user_message[1:]
+            await send_message(message, user_message, is_private=True)
+        else:
+            await send_message(message, user_message, is_private=False)
 
-// lo usa para los prefijos para que conteste uso el ?
-if user_message[0]=='?':
-    user_message=username[1:]
-    await send_message(message,user_message,is_private=True)
-else :
-    await send_message(message,user_message,is_private=False)
-
-client.run(TOKEN)   // Dice que hasta este punto el bot ya debe de jalar
-                    // el problema fue mal identado 
-
-
-
-// el get_response es lo que responde que si dice hola devuelva hola 
-// dice que esto solo sirve mientras la pc este corriendo 
-
-// el import random va en el archivo de respuestas 
-// En la parte de formacion de equipos ponerlo ahi ,enviarlo antes de la tarea
-// enlace del Bot
-debe de tener una dinamica el chat bot
-
-
+    client.run(TOKEN)
