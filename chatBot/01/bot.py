a59c50e3d11f7b9f61008aef5c3e778445dad8ac
DrJzzz <josue9499@gmail.com>
2024-02-13 15:55:00 -0600

    Adds necesary import

--- chatBot/01/bot.py
+++ chatBot/01/bot.py
@@ -1,3 +1,4 @@
+import os
 import discord
 import respuestas
 from dotenv import load_dotenv
@@ -8,8 +9,9 @@ async def send_message(message, user_message, is_private):
     response = respuestas.get_response(user_message)
     await message.author.send(response) if is_private else await message.channel.send(response)
 
+
 def run_discord_bot():
-    #For this to work you need to create a .env file an populate it with DISCORD_TOKEN=<app token>
+    # For this to work you need to create a .env file an populate it with DISCORD_TOKEN=<app token>
     TOKEN = os.getenv('DISCORD_TOKEN')
     intents = discord.Intents.default()
     intents.message_content = True
@@ -30,7 +32,7 @@ def run_discord_bot():
 
         print(f'{username} mandó: "{user_message}" ({channel})')
 
-        ##Para conversación en privado
+        # Para conversación en privado
         if user_message[0] == '?':
             user_message = user_message[1:]
             await send_message(message, user_message, is_private=True)
