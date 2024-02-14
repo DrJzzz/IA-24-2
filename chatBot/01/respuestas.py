5007748bc7b7a4aa458ef6fffc5a56879cb62376
DrJzzz <josue9499@gmail.com>
2024-02-13 18:39:46 -0600

    adds a few command-response

--- chatBot/01/respuestas.py
+++ chatBot/01/respuestas.py
@@ -1,15 +1,20 @@
 import random
+import re
+
+CTA = "!"
 
 def get_response(message: str) -> str:
 	mensaje = message.lower()
+	args_arr = message.split()
 
-	if mensaje == 'hola':
-		return '¡Hola!'
+	if mensaje[0] == '!':
+		if re.match( re.escape(CTA) + r"^def\s\w+$", message):
+			return f'La definicion de {args_arr[1]} es: ... No me la se jeje'
 
-	if mensaje == 'dado':
-		return str(random.randint(1, 6))
+		if re.match( re.escape(CTA) , r'^wiki\s+\w+(?:\s+\w+)*$' , message ):
+			return f'Articulo en wikipedia de {args_arr[1]}...'
 
-	if mensaje == 'chiste':
-		return 'No me se ninguno :c'
+		
+		return "Estas tratando de usarme? Prueba {CTA}help para saber mas de los comandos disponibles"
 
-	return 'Lo siento, no te entendí'
+	return
